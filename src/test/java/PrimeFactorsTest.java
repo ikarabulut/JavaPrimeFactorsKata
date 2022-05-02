a7290dea5aaea36aad245636ca62466cea48f056
Ismail Karabulut <i.karabulut93@gmail.com>
2022-05-02 15:26:46 -0500

    initial commit with testing framework

---
+++ src/test/java/PrimeFactorsTest.java
@@ -0,0 +1,12 @@
+package com.ikarabulut;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class PrimeFactorsTest {
+  @Test
+  void testThatJunitWorks() {
+    assertEquals(true, true);
+  }
+
+}
