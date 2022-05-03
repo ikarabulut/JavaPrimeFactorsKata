43fb4bded02a344638ea66e3573af01a379da67f
Ismail Karabulut <i.karabulut93@gmail.com>
2022-05-03 11:06:42 -0500

    completes algorithm for prime factors

--- src/main/java/PrimeFactors.java
+++ src/main/java/PrimeFactors.java
@@ -1,5 +1,19 @@
 package com.ikarabulut;
+import java.util.*;
 
 public class PrimeFactors {
-
+  
+  public static ArrayList<Integer> getPrime(Integer num) {
+    ArrayList<Integer> primeNumbers = new ArrayList<Integer>();
+    int divisor = 2;
+    while (num > 1) {
+      while (num % divisor == 0) {
+        primeNumbers.add(divisor);
+        num /= divisor;
+      }
+      divisor++;
+    }
+    return primeNumbers;
+  }
 }
+
