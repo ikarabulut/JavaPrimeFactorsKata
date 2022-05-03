a11284d95f70d0bdf97f4b0d08370e2c51e156ac
Ismail Karabulut <i.karabulut93@gmail.com>
2022-05-03 11:06:53 -0500

    adds tests through acceptance test

--- src/test/java/PrimeFactorsTest.java
+++ src/test/java/PrimeFactorsTest.java
@@ -1,5 +1,5 @@
 package com.ikarabulut;
-
+import java.util.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
@@ -7,6 +7,70 @@ public class PrimeFactorsTest {
   @Test
   void testThatJunitWorks() {
     assertEquals(true, true);
+  }
+  
+  @Test
+  void testThat2Returns2() {
+    ArrayList<Integer> prime = PrimeFactors.getPrime(2);
+    ArrayList<Integer> expected = new ArrayList<Integer>();
+    expected.add(2);
+    assertEquals(expected, prime);
+  }
+
+  @Test
+  void testThat3returns_3() {
+    ArrayList<Integer> prime = PrimeFactors.getPrime(3);
+    ArrayList<Integer> expected = new ArrayList<Integer>();
+    expected.add(3);
+    assertEquals(expected, prime);
+  }
+  
+  @Test
+  void testgetPrime_4Is2_2() {
+    ArrayList<Integer> prime = PrimeFactors.getPrime(4);
+    ArrayList<Integer> expected = new ArrayList<Integer>();
+    expected.add(2);
+    expected.add(2);
+    assertEquals(expected, prime);
+  }
+
+  @Test
+  void testGetPrime_6Is_2_3() {
+    ArrayList<Integer> prime = PrimeFactors.getPrime(6);
+    ArrayList<Integer> expected = new ArrayList<Integer>();
+    expected.add(2);
+    expected.add(3);
+    assertEquals(expected, prime);
+  }
+  
+  @Test
+  void testGetPrime8Is2_2_2() {
+    ArrayList<Integer> prime = PrimeFactors.getPrime(8);
+    ArrayList<Integer> expected = new ArrayList<Integer>();
+    expected.add(2);
+    expected.add(2);
+    expected.add(2);
+    assertEquals(expected, prime);
+  }
+
+  @Test
+  void testGetPrime9Is3_3() {
+    ArrayList<Integer> prime = PrimeFactors.getPrime(9);
+    ArrayList<Integer> expected = new ArrayList<Integer>();
+    expected.add(3);
+    expected.add(3);
+    assertEquals(expected, prime);
+  }
+  
+  @Test
+  void testGetPrimeAcceptanceTest() {
+    ArrayList<Integer> prime = PrimeFactors.getPrime(2*3*4*4);
+    ArrayList<Integer> expected = new ArrayList<Integer>();
+    expected.add(2);
+    expected.add(3);
+    expected.add(4);
+    expected.add(4);
+    assertEquals(expected, prime);
   }
 
 }
